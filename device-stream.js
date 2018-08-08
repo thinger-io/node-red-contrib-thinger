02b11d7c29425b24378de1b62cd25ea0fdc76e7c
alvarolb <alvarolb@gmail.com>
2018-08-08 13:06:08 +0200

    add device stream after refactor

---
+++ device-stream.js
@@ -0,0 +1,25 @@
+module.exports = function(RED) {
+
+    function DeviceStreamNode(config) {
+        RED.nodes.createNode(this, config);
+
+        // get node
+        var node = this;
+
+        // get server configuration
+        var server = RED.nodes.getNode(config.server);
+
+        // register listener on creation
+        server.registerDeviceResourceListener(config.device, config.resource, Number(config.interval), node);
+
+        // unregister listener on close
+        node.on('close', function(removed, done) {
+            if(removed){
+                server.unRegisterDeviceResourceListener(config.device, config.resource, node);
+            }
+            done();
+        });
+    }
+
+    RED.nodes.registerType("device-stream", DeviceStreamNode);
+};
