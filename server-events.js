2706a1e37e953903bc982eca84075ce80cdef6c0
alvarolb <alvarolb@gmail.com>
2019-06-19 18:36:22 +0200

    add server events

---
+++ server-events.js
@@ -0,0 +1,59 @@
+module.exports = function(RED) {
+    "use strict";
+    const RECONNECT_TIMEOUT_MS = 5000;
+
+    function ServerEventsNode(config) {
+        RED.nodes.createNode(this, config);
+
+        let node = this;
+        let wss = undefined;
+        let timeout = undefined;
+        let closing = false;
+
+        // get server configuration
+        let server = RED.nodes.getNode(config.server);
+
+        // fill event info
+        let event_fields = ["event", "device", "state"];
+        let event = {};
+        for (let key in config) {
+            if (event_fields.includes(key)){
+                let data = config[key].trim();
+                if(data.length!==0) event[key] = data;
+            }
+        }
+
+        function on_open(){
+            node.log("sending event subscription: " + JSON.stringify(event));
+            wss.send(JSON.stringify(event));
+        }
+
+        function on_message(payload){
+            node.send({payload: payload});
+        }
+
+        function reconnect(){
+            if(!closing){
+                timeout = setTimeout(function() {
+                    node.log("trying to reconnect");
+                    wss = server.openWebsocket(node, "/events", on_open, on_message, reconnect, reconnect);
+                }, RECONNECT_TIMEOUT_MS);
+            }else{
+                node.trace("skipping reconnection, node is closing...");
+            }
+        }
+
+        // initialize websocket connection
+        wss = server.openWebsocket(node, "/events", on_open, on_message, reconnect, reconnect);
+
+        // unregister listener on close
+        this.on('close', function() {
+            closing = true;
+            clearTimeout(timeout);
+            node.log("closing websocket connections");
+            if(wss) wss.close(null);
+        });
+    }
+
+    RED.nodes.registerType("server-events", ServerEventsNode);
+};
--- thinger-server.js
+++ thinger-server.js
@@ -6,13 +6,16 @@ const jwt = require('jsonwebtoken');
 module.exports = function(RED) {
 
     const RECONNECT_TIMEOUT_MS = 10000;
-    var devices = {};
-    var config = undefined;
-    var timeout = undefined;
+    let devices = {};
+    let config = undefined;
+    let token = undefined;
+    let timeout = undefined;
+
+    let websockets = {};
 
     function handleReconnection(){
-        for(var id in devices){
-            var device = devices[id];
+        for(let id in devices){
+            let device = devices[id];
             if(device.wss && (device.wss.readyState === device.wss.CLOSED)){
                 device.wss = getDeviceWebsocket(id);
             }
@@ -20,8 +23,8 @@ module.exports = function(RED) {
     }
 
     function closeWebsockets(){
-        for(var id in devices){
-            var device = devices[id];
+        for(let id in devices){
+            let device = devices[id];
             if(device.wss){
                 device.wss.close();
             }
@@ -30,15 +33,24 @@ module.exports = function(RED) {
 
     function ThingerServerNode(configa) {
         RED.nodes.createNode(this, configa);
-        var node = this;
+
+        let node = this;
         config = configa;
 
-        // get the decoded payload and header
-        var decoded = jwt.decode(config.token, {complete: true});
-        if(decoded && decoded.payload.usr){
-            config.username = decoded.payload.usr;
+        if(this.credentials && this.credentials.hasOwnProperty("token")) {
+            let provided_token = this.credentials.token;
+            // get the decoded payload and header
+            let decoded = jwt.decode(provided_token, {complete: true});
+            if (decoded && decoded.payload.usr) {
+                config.username = decoded.payload.usr;
+                configa.token = provided_token;
+                token = provided_token
+            } else {
+                node.error("Invalid JWT Token");
+                return;
+            }
         }else{
-            node.error("Invalid JWT Token");
+            node.error("Please, provide the Access Token");
             return;
         }
 
@@ -52,33 +64,66 @@ module.exports = function(RED) {
             done();
         });
 
+        node.openWebsocket = function(node, path, on_open, on_message, on_close, on_error){
+            const url = (config.ssl ? 'wss://' : "ws://") + config.host + "/v1/users/" + config.username + path + "?authorization=" + config.token;
+            node.log("opening websocket to: " + url);
+            let wss = new WebSocket(url);
+
+            wss.on('error', function(e){
+                node.status({fill:"red", shape:"dot", text: 'disconnected'});
+                wss.close(null);
+                if(on_error) on_error(e);
+            });
+
+            wss.on('open', function open() {
+                node.status({fill:"green", shape:"dot", text: 'connected'});
+                if(on_open) on_open();
+            });
+
+            wss.on('message', function incoming(data) {
+                // parse incoming data
+                let payload = JSON.parse(data);
+                node.status({fill:"blue",shape:"dot",text:"connected"});
+                if(on_message) on_message(payload);
+                node.status({fill:"green",shape:"dot",text:"connected"});
+            });
+
+            wss.on('close', function close() {
+                node.status({fill:"red", shape:"dot", text: 'disconnected'});
+                wss.close(null);
+                if(on_close) on_close();
+            });
+
+            return wss;
+        };
+
         node.callEndpoint = function(endpointId, data){
-            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v1/users/" + config.username + "/endpoints/" + endpointId + "/call?authorization=" + config.token;
+            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v1/users/" + config.username + "/endpoints/" + endpointId + "/call?authorization=" + token;
             request({
                 url: url,
                 method: "POST",
                 json: true,
                 body: data
             }, function (error, response, body){
-                console.log(response);
+                node.log(response);
             });
         };
 
         node.writeBucket = function(bucketId, data){
-            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v1/users/" + config.username + "/buckets/" + bucketId + "/data?authorization=" + config.token;
+            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v1/users/" + config.username + "/buckets/" + bucketId + "/data?authorization=" + token;
             request({
                 url: url,
                 method: "POST",
                 json: true,
                 body: data
             }, function (error, response, body){
-                console.log(response);
+                node.log(response);
             });
         };
 
         node.readDevice = function(deviceId, resourceId, handler){
-            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v2/users/" + config.username + "/devices/" + deviceId + "/" + resourceId + "?authorization=" + config.token;
-            console.log(url);
+            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v3/users/" + config.username + "/devices/" + deviceId + "/resources/" + resourceId + "?authorization=" + token;
+            node.log(url);
             request({
                 url: url,
                 method: "GET",
@@ -87,48 +132,48 @@ module.exports = function(RED) {
         };
 
         node.writeDevice = function(deviceId, resourceId, data){
-            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v2/users/" + config.username + "/devices/" + deviceId + "/" + resourceId + "?authorization=" + config.token;
+            const url = (config.ssl ? 'https://' : "http://") + config.host + "/v3/users/" + config.username + "/devices/" + deviceId + "/resources/" + resourceId + "?authorization=" + token;
             request({
                 url: url,
                 method: "POST",
                 json: true,
                 body: {in : data}
             }, function (error, response, body){
-                console.log(response);
+                node.log(response);
             });
         };
 
         node.unRegisterDeviceResourceListener = function (deviceId, resourceId, node){
             // check the device exists
-            var device = devices[deviceId];
+            let device = devices[deviceId];
             if(!device) return;
 
             // check the resource exists
-            var resource = device.resources[resourceId];
+            let resource = device.resources[resourceId];
             if(!resource) return;
 
             // check that the listener is on
             if(resource.listeners[node.id]){
                 delete resource.listeners[node.id];
-                console.log("Listener Cleared");
+                node.log("Listener Cleared");
 
                 controlDeviceResourceStreaming(device, resource);
 
                 // no more listeners in the resource
                 if(!Object.keys(resource.listeners).length){
                     delete device.resources[resourceId];
-                    console.log("Resource Cleared");
+                    node.log("Resource Cleared");
 
                     // device has no any resource
                     if(!Object.keys(device.resources).length){
                         // close websocket
                         if(device.wss){
-                            console.log("Websocket Closed");
+                            node.log("Websocket Closed");
                             device.wss.close();
                         }
                         // delete device
                         delete devices[deviceId];
-                        console.log("Device Cleared");
+                        node.log("Device Cleared");
 
                     }
                 }
@@ -137,7 +182,7 @@ module.exports = function(RED) {
 
         node.registerDeviceResourceListener = function(deviceId, resourceId, interval, node) {
             // initialize the device if it does not exists
-            var device = devices[deviceId];
+            let device = devices[deviceId];
             if(device === undefined){
                 device = devices[deviceId] = {
                     id : deviceId,
@@ -146,7 +191,7 @@ module.exports = function(RED) {
             }
 
             // initialize device resource
-            var resource = device.resources[resourceId];
+            let resource = device.resources[resourceId];
             if(resource === undefined){
                 resource = device.resources[resourceId] = {
                     id: resourceId,
@@ -175,14 +220,14 @@ module.exports = function(RED) {
         // there are listeners (or nodes) waiting for information
         if(Object.keys(resource.listeners).length){
             // get current resource interval
-            var currentInterval = resource.interval;
+            let currentInterval = resource.interval;
 
             // set resource interval to the greatest common divisor of all listeners
             resource.interval = getDeviceResourceInterval(device, resource);
 
             // update skip values for listeners
-            for(var id in resource.listeners){
-                var listener = resource.listeners[id];
+            for(let id in resource.listeners){
+                let listener = resource.listeners[id];
                 if(listener.interval!==resource.interval){
                     listener.skipMeasures = listener.interval>0? listener.interval  / resource.interval : 0;
                     listener.skipCurrent  = listener.interval>0? listener.interval  / resource.interval : 0;
@@ -209,7 +254,7 @@ module.exports = function(RED) {
     }
 
     function gcd(arr){
-        var i, y,
+        let i, y,
             n = arr.length,
             x = Math.abs(arr[0]);
 
@@ -224,41 +269,43 @@ module.exports = function(RED) {
     }
 
     function getDeviceResourceInterval(device, resource){
-        var intervals = [];
-        for(var id in resource.listeners){
-            var listener = resource.listeners[id];
+        let intervals = [];
+        for(let id in resource.listeners){
+            let listener = resource.listeners[id];
             intervals.push(listener.interval);
         }
-        var result =  intervals.length!==0 ? gcd(intervals) : 0;
-        console.log("Computing intervals for " + device.id + "[" + resource.id + "] with: " + JSON.stringify(intervals) + " -> " + result);
+        let result =  intervals.length!==0 ? gcd(intervals) : 0;
+        node.log("Computing intervals for " + device.id + "[" + resource.id + "] with: " + JSON.stringify(intervals) + " -> " + result);
         return result;
     }
 
     function controlDeviceResource(deviceId, control){
-        var device = devices[deviceId];
+        let device = devices[deviceId];
         if(device === undefined) return;
-        console.log(">> Sending to " + deviceId + ": " + JSON.stringify(control));
+        node.log(">> Sending to " + deviceId + ": " + JSON.stringify(control));
         device.wss.send(JSON.stringify(control));
     }
 
     function getDeviceWebsocket(deviceId){
-        const url = (config.ssl ? 'wss://' : "ws://") + config.host + "/v2/users/" + config.username + "/devices/" + deviceId + "?authorization=" + config.token;
+        const url = (config.ssl ? 'wss://' : "ws://") + config.host + "/v3/users/" + config.username + "/devices/" + deviceId + "?authorization=" + token;
 
-        console.log("opening websocket to " + url);
+        node.log("opening websocket to " + url);
 
-        var wss = new WebSocket(url);
+        let wss = new WebSocket(url);
 
         wss.on('error', function(e){
+            console.error("websocket failed");
+            console.error(e);
+
             // get current device
-            var device = devices[deviceId];
+            let device = devices[deviceId];
             if(device === undefined) return;
 
-            for(var resource_name in device.resources){
+            for(let resource_name in device.resources){
                 // mark listeners as connected
-                var resource = device.resources[resource_name];
-                for(var id in resource.listeners) {
-                    var listener = resource.listeners[id];
-                    listener.node.error("Cannot connect to Device", e.toString());
+                let resource = device.resources[resource_name];
+                for(let id in resource.listeners) {
+                    let listener = resource.listeners[id];
                     listener.node.status({fill:"red", shape:"ring", text: 'disconnected'});
                 }
             }
@@ -268,14 +315,14 @@ module.exports = function(RED) {
 
         wss.on('open', function open() {
             // get current device
-            var device = devices[deviceId];
+            let device = devices[deviceId];
             if(device === undefined) return;
 
-            for(var resource_name in device.resources){
+            for(let resource_name in device.resources){
                 // mark listeners as connected
-                var resource = device.resources[resource_name];
-                for(var id in resource.listeners) {
-                    var listener = resource.listeners[id];
+                let resource = device.resources[resource_name];
+                for(let id in resource.listeners) {
+                    let listener = resource.listeners[id];
                     listener.node.status({fill:"green",shape:"dot",text:"connected"});
                 }
                 // control resource
@@ -288,25 +335,25 @@ module.exports = function(RED) {
         });
 
         wss.on('message', function incoming(data) {
-            //console.log("<< Received from " + deviceId + ": " + data);
+            //node.log("<< Received from " + deviceId + ": " + data);
 
             // get current device
-            var device = devices[deviceId];
+            let device = devices[deviceId];
             if(device === undefined) return;
 
             // parse incoming data
-            var payload = JSON.parse(data);
+            let payload = JSON.parse(data);
 
             // get resource
-            var resource = device.resources[payload.resource];
+            let resource = device.resources[payload.resource];
 
             // no listeners for this resource?
             if(resource === undefined) return;
 
-            for(var id in resource.listeners) {
-                var listener = resource.listeners[id];
+            for(let id in resource.listeners) {
+                let listener = resource.listeners[id];
                 if(listener.skipMeasures){
-                    console.log("Listener " + id + ": SkipMeasures(" + listener.skipMeasures + ") SkipCurrent (" + listener.skipCurrent + ")");
+                    node.log("Listener " + id + ": SkipMeasures(" + listener.skipMeasures + ") SkipCurrent (" + listener.skipCurrent + ")");
                     if(listener.skipCurrent===listener.skipMeasures){
                         listener.skipCurrent = 1;
                     }else{
@@ -322,13 +369,13 @@ module.exports = function(RED) {
 
         wss.on('close', function close() {
             // get current device
-            var device = devices[deviceId];
+            let device = devices[deviceId];
             if(device === undefined) return;
-            for(var resource_name in device.resources){
+            for(let resource_name in device.resources){
                 // mark listeners as connected
-                var resource = device.resources[resource_name];
-                for(var id in resource.listeners) {
-                    var listener = resource.listeners[id];
+                let resource = device.resources[resource_name];
+                for(let id in resource.listeners) {
+                    let listener = resource.listeners[id];
                     listener.node.status({fill:"red",shape:"ring",text:"disconnected"});
                 }
             }
@@ -337,6 +384,9 @@ module.exports = function(RED) {
         return wss;
     }
 
-    RED.nodes.registerType("thinger-server", ThingerServerNode);
-
+    RED.nodes.registerType("thinger-server", ThingerServerNode, {
+        credentials: {
+            token: {type: "text"}
+        }
+    });
 };
