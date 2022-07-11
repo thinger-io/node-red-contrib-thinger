5aa03544c94efbe49bc314e8c8d45041bfb67c39
BautisFlow <jaimebs94@gmail.com>
2022-07-11 13:11:34 +0200

    Added rate limit in asset iterator node

--- nodes/asset-iterator.js
+++ nodes/asset-iterator.js
@@ -13,19 +13,39 @@ module.exports = function(RED) {
 
             if (assetType === undefined && assetGroup === undefined) {
                 newMsg.payload = res[i];
-                node.send(newMsg);
             } else if (assetType === type && assetGroup === group) {
                 newMsg.payload = res[i];
-                node.send(newMsg);
             } else if (assetType === type && assetGroup === undefined) {
                 newMsg.payload = res[i];
-                node.send(newMsg);
             } else if (assetType === undefined && assetGroup == group) {
                 newMsg.payload = res[i];
-                node.send(newMsg);
+            } else {
+                continue;
+            }
+
+            node.buffer.push(newMsg);
+
+            if (node.intervalId === -1) {
+                node.intervalId = setInterval(sendMsgFromBuffer, node.rate, node);
+                sendMsgFromBuffer(node); // send 1st message without waiting for timeout
             }
         }
     }
+
+    function sendMsgFromBuffer(node) {
+        if (node.buffer.length === 0 && typeof node.lastSent !== 'undefined') {
+          clearInterval(node.intervalId);
+          node.intervalId = -1;
+          node.lastSent = undefined;
+          node.status({fill:"blue", shape:"dot", text:"done"});
+        } else if (node.buffer.length > 0) {
+          const msg = node.buffer.shift();
+          node.lastSent = msg;
+          node.send(msg);
+          node.status({fill:"blue", shape:"ring", text:node.buffer.length});
+        }
+    }
+
 
     function AssetIteratorNode(config) {
         RED.nodes.createNode(this, config);
@@ -36,12 +56,20 @@ module.exports = function(RED) {
         // get server configuration
         var server = RED.nodes.getNode(config.server);
 
+        node.intervalId = -1;
+        node.buffer = [];
+        node.lastSent = undefined;
+        node.rate = 0;
+
         // call property read on input
-        node.on("input", async function(msg, send) {
+        node.on("input", async function(msg, send, done) {
             node.status({fill:"blue", shape:"ring", text:"running"});
 
             let asset = (config.asset || msg.asset)+"s";
             let filter = config.filter || msg.asset_filter || "";
+
+            node.rate = config.rate || msg.rate || 0;
+            node.rate = parseInt(node.rate) * 1000;
 
             let assetType = config.assetType || msg.asset_type;
             let assetGroup = config.assetGroup || msg.asset_group;
@@ -68,11 +96,17 @@ module.exports = function(RED) {
                     do {
                       const res = await server.request(node,`${url}&index=${index}`,'GET');
                       res_length = res.payload.length;
-                      handleResponse(node, res.payload, msg, assetType, assetGroup); // TODO
+                      handleResponse(node, res.payload, msg, assetType, assetGroup);
                       index += res_length;
                     } while (res_length == count);
 
-                    node.status({fill:"blue", shape:"dot", text:"done"});
+                    if (index === 0) {
+                        clearInterval(node.intervalId);
+                        node.intervalId = -1;
+                        node.status({fill:"blue", shape:"dot", text:"done"});
+                    } else {
+                        node.status({fill:"blue", shape:"ring", text:node.buffer.length});
+                    }
                 } catch(e) {
                     node.status({fill:"red", shape:"dot", text:"error"});
                     node.error(e);
